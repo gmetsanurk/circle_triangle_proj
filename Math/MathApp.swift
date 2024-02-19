b1705bbc54459e40312515ceda3f8c3483c20763
Georgy Metsanurk <gmetsanurk@gmail.com>
2024-02-19 20:09:33 +0300

    Initial Commit

---
+++ Math/MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathApp.swift
+//  Math
+//
+//  Created by Georgy on 2024-02-19.
+//
+
+import SwiftUI
+
+@main
+struct MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
